c73e835570a53b3686d3c6318be109fbcff5efb2
Abdurrehman050 <abdurrehmanfsd07@gmail.com>
2024-06-12 18:17:50 +0500

    projects

--- MERN-AI-ChatGpt-Content-Generator/backend/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const app = express();
-const PORT = process.env.PORT || 8090;
-
-//! Start server
-app.listen(PORT, console.log(`server is running on ${PORT}`));
